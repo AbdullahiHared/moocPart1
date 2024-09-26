d34a201a45effc3cf24ad21004d076843ebc2c7f
Abdullah Hared <muxsinxareed6@gmail.com>
2024-09-26 10:51:23 +0200

    create a timer

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,23 @@
+import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Books> books = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        Timer timer = new Timer();
 
-        // ask user for personal information
         while (true) {
-            System.out.println("Please Enter the book title");
-            String bookTitle = sc.nextLine();
+            System.out.println(timer);
+            timer.advance();
 
-            // Check if the name of the book is empty to stop the loop
-            if (bookTitle.isEmpty()) {
-                break;
-            }
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
 
-            System.out.println("Book pages? ");
-            int bookPages = sc.nextInt();
-            sc.nextLine();  // Consume the newline after entering the integer
-
-            System.out.println("Book publication year? ");
-            int publishYear = sc.nextInt();
-            sc.nextLine();  // Consume the newline after entering the integer
-
-            // Add new PersonalInformation object with firstName, lastName, and id
-            books.add(new Books(bookTitle, bookPages, publishYear ));
-        }
-
-        System.out.println("Please choose what to print ");
-         String userChoice = sc.nextLine().toLowerCase();
-
-        // Print all television programs that fulfill the maximum duration
-        for (Books book : books) {
-            switch (userChoice) {
-                case "everything" -> System.out.println(book.printEverything());
-                case "title" -> System.out.println(book.printTitle());
-                case "year" -> System.out.println(book.printPublicationYear());
             }
         }
 
-        sc.close();  // Close the scanner to prevent resource leak
     }
 }
