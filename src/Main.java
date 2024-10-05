5b5c5834b6a5a9b6748c802ae323b3ebecc80519
Abdullah Hared <muxsinxareed6@gmail.com>
2024-10-05 06:33:27 +0200

    Separating the user interface from the program logic

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,11 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        PaymentTerminal unicafeExactum = new PaymentTerminal();
-        System.out.println(unicafeExactum);
+        WordSet set = new WordSet();
 
-        PaymentCard annesCard = new PaymentCard(2);
-
-        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
-
-        boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
-        System.out.println();
-
-        unicafeExactum.addMoneyToCard(annesCard, 100);
-        System.out.println(annesCard.balance());
-
-        wasSuccessful = unicafeExactum.eatHeartily(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
-
-        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
-
-        System.out.println(unicafeExactum);
+        UserInterface userInterface = new UserInterface(set, scanner);
+        userInterface.start();
     }
 }
