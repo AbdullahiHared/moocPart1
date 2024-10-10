cb2f11edff5f8f8817022c1d3ba0dc83f6865ec7
Abdullah Hared <muxsinxareed6@gmail.com>
2024-10-10 10:42:13 +0200

    grade statistics

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        SimpleDictionary simpleDictionary = new SimpleDictionary();
-        TextUi textUi = new TextUi(scanner, simpleDictionary);
-        textUi.start();
+        GradesStatistic gradesStatistic = new GradesStatistic();
+        gradesStatistic.start();
     }
 }
