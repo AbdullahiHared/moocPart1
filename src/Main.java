be08625abacdd66c386dea21f71cf29043c70196
Abdullah Hared <muxsinxareed6@gmail.com>
2024-10-16 20:19:04 +0200

    Start working with hashMaps

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        GradesStatistic gradesStatistic = new GradesStatistic();
-        gradesStatistic.start();
+        HashMap<String, Book> directory = new HashMap<>();
+        Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "The family of Dashwood had long been settled in Sussex.");
+        Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "It is a truth universally acknowledged, that a ...");
+
+        directory.put(senseAndSensibility.getName(), senseAndSensibility);
+        directory.put(prideAndPrejudice.getName(), prideAndPrejudice);
+
+
+        Book book = directory.get("Persuasion");
+        System.out.println(book);
+        System.out.println();
+        book = directory.get("Pride and Prejudice");
+        System.out.println(book);
     }
 }
